6cc5cdd81a843dca2b2a93d99bf603c7cea75b35
rogerangel <40092517+rogerangel@users.noreply.github.com>
2023-10-12 14:33:01 -0400

    testing imgage built-in optimization

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/AvianDS",
-  images: {
-    loader: "custom",
-    loaderFile: "./my-loader.js",
-  },
+  // output: "export",
+  // basePath: "/letrasinc",
+  // images: {
+  //   loader: "custom",
+  //   loaderFile: "./my-loader.js",
+  // },
 };
 
 module.exports = nextConfig;
