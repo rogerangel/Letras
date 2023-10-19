62a20a367d516fae3bc70d97d19503e72eb9a6fe
rogerangel <40092517+rogerangel@users.noreply.github.com>
2023-10-19 12:01:54 -0400

    Hero content box to cover whole img

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -20,28 +20,45 @@ export default function Hero() {
   });
   return (
     <div className="relative">
-      <Image
-        src={kids_tiles}
-        alt="Tiles of kids reading"
-        className="img w-full h-auto min-h-[550px] object-cover"
-        priority
-      />
+      <motion.div
+        ref={ref}
+        initial={{ opacity: 0, scale: 1.5 }}
+        animate={{ opacity: inView ? 1 : 0, scale: inView ? 1 : 0.5 }}
+        // initial={{ x: -300, opacity: 0, scale: 0.1 }}
+        // animate={{ x: 0, opacity: inView ? 1 : 0, scale: inView ? 1 : 0.5 }}
+        transition={{
+          duration: 1.9,
+          delay: 0.0,
+          ease: [0, 0.71, 0.2, 1.01],
+        }}
+      >
+        <Image
+          src={kids_tiles}
+          alt="Tiles of kids reading"
+          className="img w-full h-auto min-h-[550px] object-cover"
+          priority
+        />
+      </motion.div>
       <AnimatePresence>
         <motion.div
           ref={ref}
-          initial={{ x: -300, opacity: 0, scale: 0.1 }}
-          animate={{ x: 0, opacity: inView ? 1 : 0, scale: inView ? 1 : 0.5 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: inView ? 1 : 0 }}
+          // initial={{ x: -300, opacity: 0, scale: 0.1 }}
+          // animate={{ x: 0, opacity: inView ? 1 : 0, scale: inView ? 1 : 0.5 }}
           transition={{
-            duration: 0.8,
-            delay: 0.7,
+            duration: 1.9,
+            delay: 1.7,
             ease: [0, 0.71, 0.2, 1.01],
           }}
-          className="absolute top-0 h-full flex flex-col justify-around md:justify-evenly items-center gap-1 md:top-[12.25%] md:left-[5%] md:w-[38%] md:h-[72%] bg-stone-700 md:bg-stone-600 rounded-lg shadow-2xl px-3 bg-opacity-70 md:bg-opacity-90 md:backdrop-blur-sm"
+          // className="absolute top-0 h-full flex flex-col justify-around md:justify-evenly items-center gap-1 md:top-[12.25%] md:left-[5%] md:w-[38%] md:h-[72%] bg-stone-700 md:bg-stone-600 rounded-lg shadow-2xl px-3 bg-opacity-70 md:bg-opacity-90 md:backdrop-blur-sm"
+          className="absolute top-0 h-full flex flex-col justify-around md:justify-evenly items-center bg-stone-900 shadow-2xl px-3 bg-opacity-70 md:bg-opacity-70"
+        // className="h-full flex flex-col justify-around md:justify-evenly items-center bg-stone-900 shadow-2xl px-3 bg-opacity-70 md:bg-opacity-70"
         >
           <h1
             className={classNames(
               LogoFont.className,
-              "text-4xl lg:text-5xl text-center md:text-left md:px-5 font-extrabold md:font-bold md:text-emerald-500 text-emerald-400"
+              "text-4xl lg:text-5xl text-center md:text-left md:px-5 font-extrabold md:font-bold text-emerald-500"
             )}
           >
             Little Learners Need Your Help!
