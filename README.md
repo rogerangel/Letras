0aa883418f1c767b80de36a44014d9895bfafbcc
rogerangel <40092517+rogerangel@users.noreply.github.com>
2024-03-10 04:05:42 -0400

    ig comp, email temp, inview enhancemnts

--- README.md
+++
@@ -1,3 +0,0 @@
-This page is being develop for the Letras Inc. Organization.
-
-Letras Inc. is a non-profit organization that aims to promote the development of the arts and culture Worldwide.
--- src/components/IG_Embed.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { InstagramEmbed } from "react-social-media-embed";
-
-export default function IG_Embed(props) {
-  return (
-    <InstagramEmbed {...props} url="https://www.instagram.com/letras.nyc/" />
-  );
-}
